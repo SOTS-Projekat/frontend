df1a5179f9843390cd24b4ef5723c658600f650b
DusanVukovic3 <vukoviduan047@gmail.com>
2024-10-29 17:59:38 +0100

    need to add CORS

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,24 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
-
+import LoginService from "./Services/LoginService";
 
 const LoginForm = () => {
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setError(null); 
+
+        try {
+            const token = await LoginService.login(username, password);
+            console.log("Login successful, token:", token);
+            localStorage.setItem('token', token);
+
+            setUsername("");
+            setPassword("");
+        } catch (err) {
+            setError(err.message); // Display the error message to the user
+        }
+    };
+
     return (
         <div className="wrapper">
-            <form action="">
+            <form onSubmit={handleLogin}>
                 <h1>
                     Login
                 </h1>
                 <div className="input-box">
-                    <input type="text" placeholder="Username" required />
+                    <input type="text" placeholder="Username" value={username} onChange={(e => setUsername(e.target.value))} required />
                     <FaUser className="icon"/>
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required />
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                     <FaLock className="icon"/>
                 </div>
                 <button type="submit">Login</button>
+
+                {error && <p className="error-message">{error}</p>}
 
                 <div className="register-link">
                     <p>Don't have an account? <a href="#">Register</a></p>
