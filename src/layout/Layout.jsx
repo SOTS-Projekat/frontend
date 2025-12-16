a63155fd314957e6b14832eba16d62cd6d4490a1
DusanVukovic3 <vukoviduan047@gmail.com>
2025-12-16 19:45:58 +0100

    .

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navigation from "../components/Navigation/Navigation";
+import Navigation from "../Components/Navigation/Navigation";
 import classes from "./Layout.module.scss";
 import { ToastContainer } from "react-toastify";
 
